870e8af10ec99b7b18646abd7149786cf67d693a
Alberto Leal <mail4alberto@gmail.com>
2024-06-03 18:41:07 -0400

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::thread;
-use std::time::Duration;
-
 use rayon::prelude::*;
 use sentry::protocol::Request;
+use sentry::protocol::{Event, Level};
+use sentry::types::random_uuid;
+use sentry::TransactionOrSpan;
+use std::thread;
+use std::time::Duration;
 
 // cargo run --example performance-demo
 fn main() {
@@ -56,7 +58,7 @@ fn generate_transactions(x: i64) {
 }
 
 fn main_span1() {
-    wrap_in_span("span1", "", || {
+    wrap_in_span("span1", "", |_: &TransactionOrSpan| {
         thread::sleep(Duration::from_millis(5));
 
         let transaction_ctx = sentry::TransactionContext::continue_from_span(
@@ -82,13 +84,30 @@ fn main_span1() {
 }
 
 fn thread_span1() {
-    wrap_in_span("span1", "", || {
+    wrap_in_span("span1", "", |_: &TransactionOrSpan| {
         thread::sleep(Duration::from_millis(20));
-    })
+    });
+
+    wrap_in_span(
+        "db.query",
+        "SELECT * FROM users WHERE id = %s",
+        |span: &TransactionOrSpan| {
+            let uuid = random_uuid();
+            sentry::capture_event(Event {
+                event_id: uuid,
+                message: Some("Some error".into()),
+                level: Level::Error,
+                ..Default::default()
+            });
+
+            span.set_data("db.system", "postgresql".into());
+            thread::sleep(Duration::from_millis(5));
+        },
+    );
 }
 
 fn main_span2() {
-    wrap_in_span("span2", "", || {
+    wrap_in_span("span2", "", |_: &TransactionOrSpan| {
         sentry::capture_message(
             "A message that should have a trace context",
             sentry::Level::Info,
@@ -99,7 +118,7 @@ fn main_span2() {
 
 fn wrap_in_span<F, R>(op: &str, description: &str, f: F) -> R
 where
-    F: FnOnce() -> R,
+    F: FnOnce(&TransactionOrSpan) -> R,
 {
     let parent = sentry::configure_scope(|scope| scope.get_span());
     let span1: sentry::TransactionOrSpan = match &parent {
@@ -117,7 +136,7 @@ where
     span1.set_request(span_request);
     sentry::configure_scope(|scope| scope.set_span(Some(span1.clone())));
 
-    let rv = f();
+    let rv = f(&span1);
 
     span1.finish();
     sentry::configure_scope(|scope| scope.set_span(parent));
